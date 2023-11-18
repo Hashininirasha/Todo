3ef910c05e4a697d669175f077cad9ae1146c445
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2023-11-19 00:57:13 +0530

    added BE list and listcontext

---
+++ asp.net-backend/asp.net-backend/Controllers/Models/List.cs
@@ -0,0 +1,12 @@
+﻿namespace asp.net_backend.Controllers.Models
+{
+    public class List
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public Boolean IsActive { get; set; }
+    }
+}
---
+++ asp.net-backend/asp.net-backend/Controllers/Models/ListContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace asp.net_backend.Controllers.Models
+{
+    public class ListContext : DbContext
+    {
+        public ListContext(DbContextOptions<ListContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<List> Lists { get; set; }
+
+    }
+}
